18efefe8602ecc293cded0f27226e03d9d7e6962
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2023-01-07 21:03:10 +0000

    debug

--- test_main.py
+++ test_main.py
@@ -1,9 +1,10 @@
+import os
 import sqlalchemy as sa
 from main import initdb
 
 
 def test_initdb():
-    root_engine = sa.create_engine("postgresql+psycopg2://postgres:postgres@postgres")
+    root_engine = sa.create_engine("postgresql+psycopg2://postgres:postgres@" + os.getenv("POSTGRES_HOST"))
     engine = initdb(root_engine)
     with engine.begin():
         assert True
