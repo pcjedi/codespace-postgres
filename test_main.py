f402b5f20ef3f7675551de46f0125c5c59d95053
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2023-01-07 20:50:33 +0000

    agnositic

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import initdb
 
 
 def test_initdb():
-    root_engine = sa.create_engine(name_or_url="postgresql+psycopg2://postgres:postgres@localhost")
+    root_engine = sa.create_engine("postgresql+psycopg2://postgres:postgres@localhost")
     engine = initdb(root_engine)
     with engine.begin():
         assert True
